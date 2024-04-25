621a32eb2938e03e2b3f9282ec519c18de0e7995
https://github.com/igorfonseca05 <78857547+igorfonseca05@users.noreply.github.com>
2024-04-25 10:24:36 -0300

    atualizando configuração do cors

--- api.js
+++ api.js
@@ -11,17 +11,6 @@ global.app = app
 // Carregando o cors para podemos obter requisições a nossa API de qualquer origem/dominio
 app.use(cors())
 
-app.use((req, res, next) => {
-    // res.setHeader('Access-Control-Allow-Origin', 'http://127.0.1.0:5173'); // Permitindo qualquer origem. Você pode substituir '*' pelo seu domínio específico.
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Métodos permitidos
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Cabeçalhos permitidos
-    if (req.method === 'OPTIONS') {
-        res.sendStatus(200); // Responde com 200 para solicitações OPTIONS
-    } else {
-        next();
-    }
-});
-
 // Importando rotas
 const router = require('./routes/routes')
 

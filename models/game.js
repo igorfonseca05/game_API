c86ffe3dc1db73389bad727d562a4853e4db3590
https://github.com/igorfonseca05 <78857547+igorfonseca05@users.noreply.github.com>
2025-01-22 16:01:38 -0300

    Adicionei senha String de conexão

--- models/game.js
+++ models/game.js
@@ -9,20 +9,20 @@ mongoose.connect(process.env.MONGO_ATLAS_URL)
         console.log('base conectada')
     })
     .catch((erro) => {
-       console.log(erro.message)
+        console.log(erro.message)
     })
 
 
 const gameShema = new mongoose.Schema({
-    name: { type: String},
-    description: {type: String},
+    name: { type: String },
+    description: { type: String },
     tags: [String],
-    platform: { type: mongoose.Schema.Types.Mixed},
-    steam_link: {type: String, default: ''},
-    price: {type: mongoose.Schema.Types.Mixed},
-    createdAt: {type: Date, default: new Date().getTime()}
-  })
+    platform: { type: mongoose.Schema.Types.Mixed },
+    steam_link: { type: String, default: '' },
+    price: { type: mongoose.Schema.Types.Mixed },
+    createdAt: { type: Date, default: new Date().getTime() }
+})
 
-  const game = mongoose.model('game', gameShema, "game")
+const game = mongoose.model('game', gameShema, "game")
 
-  module.exports = game
+module.exports = game
